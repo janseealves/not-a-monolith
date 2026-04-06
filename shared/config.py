584712f69b9b16cd09b3a6345b43ab4532a3bdf8
jansee <jansen.alvesr@gmail.com>
2026-04-06 19:25:21 -0300

    feat: add configuration settings using Pydantic

---
+++ shared/config.py
@@ -0,0 +1,14 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "NOT A PROJECT"
+    DATABASE_URL: str | None = None
+    LLM_API_KEY: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+settings = Settings()
