8fbd9c2a81c49eadb67c2003cd92a96f83c9f059
jansee <jansen.alvesr@gmail.com>
2026-06-01 19:26:17 -0300

    refactor: update vector_store attribute to be private in SemanticRetriever and HybridRetriever

--- modules/rag/retrieval/retriever.py
+++ modules/rag/retrieval/retriever.py
@@ -9,11 +9,11 @@ logger = logging.getLogger(__name__)
 
 class SemanticRetriever(BaseRetriever):
     def __init__(self, vector_store: VectorStore):
-        self.vector_store = vector_store
+        self._vector_store = vector_store
 
     def retrieve(self, query: str, top_k: int = 5) -> list[RetrievedDocument]:
         logger.info("Retrieving top %d chunks for query: '%s'", top_k, query)
-        result = self.vector_store.similarity_search_with_score(query, k=top_k)
+        result = self._vector_store.similarity_search_with_score(query, k=top_k)
 
         if not result:
             logger.warning("No chunks retrieved.")
@@ -24,7 +24,7 @@ class SemanticRetriever(BaseRetriever):
 
 class HybridRetriever(BaseRetriever):
     def __init__(self, vector_store: VectorStore):
-        self.vector_store = vector_store
+        self._vector_store = vector_store
 
     def retrieve(self, query: str, top_k: int = 5) -> list[RetrievedDocument]:
         pass
